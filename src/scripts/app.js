0bf0d1d42c9b782697f3fc7d01f838f1dd560e95
Danicadel29 <danica.delfosse@student.heaj.be>
2025-05-15 14:39:18 +0200

    essais cursor

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,32 +1,32 @@
-/* /* Curseur personnalisé sur l'état hover des img--schema 
+//  Curseur personnalisé sur l'état hover des img--schema //
 const body = document.body;
-const customCursor = document.querySelector('.custom-cursor');
-const imgschema = document.querySelectorAll('.img--schema');
+const Cursor = document.querySelector('.cursor');
+const images = document.querySelectorAll('.cursor-img');
 
 function moveCursor(e) {
     console.log('mousemove');
-  customCursor.style.top = `${(e.clientY + window.scrollY) - 32}px`; // 32 c'est la moitié de la taille du curseur, pour le centrer, si tu veux que le "click" soit en haut à gauche, comme pour la souris, alors retire le 32
-  customCursor.style.left = `${e.clientX - 32}px`;
+  Cursor.style.top = `${(e.clientY + window.scrollY) - 32}px`; // 32 c'est la moitié de la taille du curseur, pour le centrer, si tu veux que le "click" soit en haut à gauche, comme pour la souris, alors retire le 32
+  Cursor.style.left = `${e.clientX - 32}px`;
 }
 
 if (window.innerWidth >= 1280) {
-    imgschema.forEach((img) => {
+    images.forEach((img) => {
         img.addEventListener('mouseenter', () => {
             console.log('mouseenter');
-        customCursor.style.visibility = 'visible';
+        Cursor.style.visibility = 'visible';
         body.style.cursor = 'none';
         img.addEventListener('mousemove', moveCursor);
         });
     });
 
-    imgschema.forEach((img) => {
+    images.forEach((img) => {
         img.addEventListener('mouseleave', (e) => {
             console.log('mouseleave');
         if (!img.contains(e.relatedTarget)) {
-            customCursor.style.visibility = 'hidden';
+            Cursor.style.visibility = 'hidden';
             body.style.cursor = 'default';
             img.removeEventListener('mousemove', moveCursor);
         }
     });
 });
-} */
+}
